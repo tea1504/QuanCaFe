1dbecd1c9576f545cfdff015a0447d84b8ce2508
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-13 22:18:58 +0700

    Chức năng quản lý bàn

--- QuanCaFe.WebApp/Controllers/BanController.cs
+++ QuanCaFe.WebApp/Controllers/BanController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuanCaFe.Data.EF;
+using QuanCaFe.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,57 @@ namespace QuanCaFe.WebApp.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Bans.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            ViewBag.id = _context.Bans.Count() + 1;
+            return View();
+        }
+        [HttpPost, ActionName("Create")]
+        public IActionResult Create(Ban b)
+        {
+            try
+            {
+                _context.Bans.Add(b);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                ViewBag.Err = "Lỗi lưu dữ liệu";
+                return View("Create", b);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            Ban b = _context.Bans.Find(id);
+            return View(b);
+        }
+        [HttpPost, ActionName("Edit")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var banToUpdate = await _context.Bans.SingleOrDefaultAsync(b => b.Id == id);
+            try
+            {
+                if (await TryUpdateModelAsync<Ban>(banToUpdate, "", b=>b.Mota))
+                {
+                    _context.Entry(banToUpdate).State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Err = "Lỗi lưu dữ liệu";
+                return View("Edit", banToUpdate);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Ban b = _context.Bans.SingleOrDefault(b => b.Id == id);
+            _context.Remove(b);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
